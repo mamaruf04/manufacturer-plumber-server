e2d0f1335d5469e0c3f917ca859cddd6621ef3da
mamaruf04 <mamar@DESKTOP-AR6EM10.(none)>
2022-11-10 12:50:21 +0600

    delete my order

--- index.js
+++ index.js
@@ -106,6 +106,14 @@ async function run() {
 
         })
 
+        //13 delete my order
+        app.delete('/order/:email', async (req, res) => {
+            const email = req.params.email
+            const filter = { email: email }
+            const result = await ordersCollection.deleteOne(filter)
+            res.send(result)
+        })
+
     }
     finally {
         //   await client.close();
