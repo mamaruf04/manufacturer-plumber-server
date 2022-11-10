1eb89c2ddf7c7fce7cb99dcd8b15ccf61c29053b
mamaruf04 <mamar@DESKTOP-AR6EM10.(none)>
2022-11-10 13:07:55 +0600

    shippeted

--- index.js
+++ index.js
@@ -168,6 +168,17 @@ async function run() {
             res.send(result)
         })
 
+        //33 shipped
+        app.put('/all-order/order/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id : ObjectId(id)};
+            const updateDoc = {
+                $set: { process: 'Shipped' }
+            };
+            const result = await ordersCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
         // ***    User        **//
 
         //15  user create or update 
