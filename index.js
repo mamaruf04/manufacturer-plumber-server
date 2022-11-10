3840c56fa27e525b867ee88dd70e1f30a407137f
mamaruf04 <mamar@DESKTOP-AR6EM10.(none)>
2022-11-10 12:16:36 +0600

    Db connected

--- index.js
+++ index.js
@@ -7,6 +7,21 @@ const jwt = require('jsonwebtoken');
 const stripe = require('stripe')('sk_test_51L14pjDEsxnXfJbTlrS3grchkKNLNJquxxzz79aQiElQwp6RcnTeEJIRskV7INrmUt7vBTFS2pWMTokjKFP0nbIC00bPMze6Az')
 
 
+//3
+const cors = require('cors')
+require('dotenv').config()
+
+//4
+app.use(cors())
+app.use(express.json())
+
+//5 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ajito.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// console.log(uri);
+
 //2
 app.get('/', (req, res) => {
     res.send('Plumbing server is running')
